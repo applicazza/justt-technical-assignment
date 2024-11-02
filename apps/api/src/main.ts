d8b2bfee64acc17ebafea9eba812735f7307ce87
Artyom Sokolov <artyom@applicazza.com>
2024-11-03 00:54:41 +0200

    add Swagger integration for Blog API; update main.ts and package.json

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { ApiModule } from './api.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('Blog API')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const config = app.get(ConfigService);
 
